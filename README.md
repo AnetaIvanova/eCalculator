cbf4f11a584862b706aa3e00ffd28f17d9ec69d5
Aneta Ivanova <ai00327@surrey.ac.uk>
2021-06-06 15:30:17 +0100

    tests fixed

--- README.md
+++ README.md
@@ -21,16 +21,19 @@ Description:
 
 //////////////////////////////////////////////////////////////////////////////
 
-The following technologies were used:
-
+--The following technologies were used:
 #HTML - to build the structure of the program.
 #CSS - to add styles to the program display.
 #JavaScript - to perform the logic of the user's operations when using the calculator.
 #VSCode 1.56.2 on MacOs 64bit.
 
-How to run the "eCalculator":
-
+--How to run the "eCalculator":
 #Download the 3 files of code.
 #Open the index.html file in your browser and run it.
+
+--How to run the tests in cypress:
+
+1. install cypress: npm install cypress --save-dev
+2. npm run e2e. e2e is the name of the script, given in package.json file.
 
 //////////////////////////////////////////////////////////////////////////////
--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -1,19 +1,61 @@
+//install cypress:npm install cypress --save-dev
+//run it: npm run e2e <e2e is the name of the script>, see package.json file
+
+// Test if the calculator displays on a browser
 describe("My first test", () => {
   it("Should visit our calculator", () => {
     cy.visit("http://127.0.0.1:5500/index.html");
   });
-  //   it("Should contain 0", () => {
-  //     cy.get(".zero").contains("0");
-  //   });
+  it("Should contain 0", () => {
+    cy.get(".zero").contains("0");
+  });
 });
 
-//"...." are the name of the classes
-// cy.get('[data-cy=submit]').click()
+// Test "+" operator
 describe("Addition operator", () => {
   it("Should equal 10", () => {
-    cy.get(".digit button five").click();
-    cy.get(".operation button add").click();
-    cy.get(".digit button five").click();
-    cy.get(".span-two button").click();
+    cy.get(".five").click();
+    cy.get(".add").click();
+    cy.get(".five").click();
+    cy.get(".equals").click();
+    cy.get(".output").should("have.value", 10);
+  });
+});
+
+// Test "/" operator
+describe("Division operator", () => {
+  it("Should equal 5", () => {
+    cy.get(".two").click();
+    cy.get(".five").click();
+    cy.get(".division").click();
+    cy.get(".five").click();
+    cy.get(".equals").click();
+    cy.get(".output").should("have.value", 5);
+  });
+});
+
+// Test "*" operator
+describe("Multiplication operator", () => {
+  it("Should equal 150", () => {
+    cy.get(".one").click();
+    cy.get(".five").click();
+    cy.get(".mult").click();
+    cy.get(".one").click();
+    cy.get(".zero").click();
+    cy.get(".equals").click();
+    cy.get(".output").should("have.value", 150);
+  });
+});
+
+// Test "-" operator
+describe("Subtraction operator", () => {
+  it("Should equal 30", () => {
+    cy.get(".six").click();
+    cy.get(".zero").click();
+    cy.get(".minus").click();
+    cy.get(".three").click();
+    cy.get(".zero").click();
+    cy.get(".equals").click();
+    cy.get(".output").should("have.value", 30);
   });
 });
